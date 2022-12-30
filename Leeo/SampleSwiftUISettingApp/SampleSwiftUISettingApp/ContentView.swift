608b3e30ff1388e6c42dfae07d5b5bbe81e3c866
sei <vanism7557@gmail.com>
2022-12-30 23:24:24 +0900

    study: Leeo SwiftUI setting app 5-refactoring2

--- Leeo/SampleSwiftUISettingApp/SampleSwiftUISettingApp/ContentView.swift
+++ Leeo/SampleSwiftUISettingApp/SampleSwiftUISettingApp/ContentView.swift
@@ -22,55 +22,34 @@ struct ContentView: View {
                 }
 
                 Section {
-                    HStack {
-                        NavigationLink("Apple Arcade 3개월 무료") {
-                            Text("Apple Arcade 3개월 무료")
-                        }
-                    }
-                }
-
-                Section {
-                    NavigationLink{
-                        Text("마저하기 설정 화면")
-                    } label: {
-                        HStack{
-                            Text("iPhone 설정 마저 하기")
-                                .badge(1)
-                        }
-                    }
-                }
-
-                Section {
                     toggleCell(imageName: "airplane",
-//                               imageBackgroundColor: .orange,
                                iconColor: .orange,
                                cellTitle: "에어플레인 모드",
                                toggleBind: $isAirplaneModeOn
                     )
-                    textCell(imageName: "wifi",
-                             imageBackgroundColor: .blue,
+                    navigationLinkCell(imageName: "wifi",
+                             iconColor: .blue,
                              cellTitle: "Wi-Fi",
-                             description: "SBA_YongSan") {
+                             subTitle: "SBA_YongSan") {
                         Text("Wifi 설정 화면")
                     }
-                    textCell(imageName: "point.3.filled.connected.trianglepath.dotted",
-                             imageBackgroundColor: .indigo,
+                    navigationLinkCell(imageName: "point.3.filled.connected.trianglepath.dotted",
+                             iconColor: .indigo,
                              cellTitle: "Bluetooth",
-                             description: "켬") {
+                             subTitle: "켬") {
                         Text("BT 설정 화면")
                     }
-                    plainCell(imageName: "dot.radiowaves.left.and.right",
-                              imageBackgroundColor: .green,
+                    navigationLinkCell(imageName: "dot.radiowaves.left.and.right",
+                              iconColor: .green,
                               cellTitle: "셀룰러") {
                         Text("셀룰러 화면")
                     }
-                    plainCell(imageName: "personalhotspot",
-                              imageBackgroundColor: .green,
+                    navigationLinkCell(imageName: "personalhotspot",
+                              iconColor: .green,
                               cellTitle: "개인용 핫스팟") {
                         Text("개인용 핫스팟 화면")
                     }
                     toggleCell(imageName: "lock.icloud",
-//                               imageBackgroundColor: .blue,
                                iconColor: .blue,
                                cellTitle: "VPN",
                                toggleBind: $isVPNOn
@@ -78,53 +57,24 @@ struct ContentView: View {
                 }
                 
                 Section {
-                    plainCell(imageName: "hourglass",
-                              imageBackgroundColor: .indigo,
+                    navigationLinkCell(imageName: "hourglass",
+                              iconColor: .indigo,
                               cellTitle: "스크린타임") {
                         Text("스크린타임 설정 화면")
                     }
                 }
                 Section {
-                    HStack {
-                        Image(systemName: "gearshape.fill")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 20, height: 20)
-                            .padding(.all, 4)
-                            .background(.gray)
-                            .foregroundColor(.white)
-                            .cornerRadius(5)
-                        NavigationLink("일반") {
-                            Text("일반 화면")
-                        }
+                    navigationLinkCell(imageName: "gearshape.fill",
+                                       iconColor: .gray,
+                                       cellTitle: "일반") {
+                        Text("일반 화면")
                     }
-                    HStack {
-                        Image(systemName: "figure.wave.circle")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 20, height: 20)
-                            .padding(.all, 4)
-                            .background(.blue)
-                            .foregroundColor(.white)
-                            .cornerRadius(5)
-                        NavigationLink("손쉬운 사용") {
-                            Text("손쉬운 사용 화면")
-                        }
+                   
+                    navigationLinkCell(imageName: "figure.wave.circle",
+                                       iconColor: .blue,
+                                       cellTitle: "손쉬운 사용") {
+                        Text("손쉬운 사용 화면")
                     }
-                    HStack {
-                        Image(systemName: "hourglass")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 20, height: 20)
-                            .padding(.all, 4)
-                            .background(.indigo)
-                            .foregroundColor(.white)
-                            .cornerRadius(5)
-                        NavigationLink("스크린타임") {
-                            Text("스크린 타임 화면")
-                        }
-                    }
-                    Text("hello")
                 }
             }
             .navigationTitle("설정")
@@ -158,61 +108,43 @@ struct ContentView: View {
     }
     
     @ViewBuilder
-//    private func toggleCell<S: ShapeStyle>(imageName:String, imageBackgroundColor: S, cellTitle: String, toggleBind: Binding<Bool>) -> some View {
+    private func cellIcon(imageName: String, iconColor: Color) -> some View {
+        Image(systemName: imageName)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 20, height: 20)
+            .padding(.all, 4)
+            .background(iconColor)
+            .foregroundColor(.white)
+            .cornerRadius(5)
+    }
+    
+    @ViewBuilder
     private func toggleCell(imageName:String, iconColor: Color, cellTitle: String, toggleBind: Binding<Bool>) -> some View {
         HStack {
-            Image(systemName: imageName)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 20, height: 20)
-                .padding(.all, 4)
-                .background(iconColor)
-                .foregroundColor(.white)
-                .cornerRadius(5)
+            cellIcon(imageName: imageName, iconColor: iconColor)
             
             Toggle(cellTitle,
                    isOn: toggleBind)
         }
     }
-    
+
     @ViewBuilder
-    private func textCell<V:View, S: ShapeStyle>(imageName: String, imageBackgroundColor: S, cellTitle: String, description: String, destination: @escaping ()->V) -> some View {
+    private func navigationLinkCell<V:View>(imageName: String, iconColor: Color, cellTitle: String, subTitle: String? = nil, destination: @escaping ()->V) -> some View {
         HStack {
-            Image(systemName: imageName)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 20, height: 20)
-                .padding(.all, 4)
-                .background(imageBackgroundColor)
-                .foregroundColor(.white)
-                .cornerRadius(5)
+            cellIcon(imageName: imageName, iconColor: iconColor)
             
             NavigationLink{
                 destination()
             } label: {
                 HStack {
                     Text(cellTitle)
-                    Spacer()
-                    Text(description)
-                        .foregroundColor(.gray)
+                    if let subTitle {
+                        Spacer()
+                        Text(subTitle)
+                            .foregroundColor(.gray)
+                    }
                 }
-            }
-        }
-    }
-    
-    @ViewBuilder
-    private func plainCell<V: View, S: ShapeStyle>(imageName: String, imageBackgroundColor: S, cellTitle: String, destination: @escaping ()->V) -> some View {
-        HStack {
-            Image(systemName: imageName)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 20, height: 20)
-                .padding(.all, 4)
-                .background(imageBackgroundColor)
-                .foregroundColor(.white)
-                .cornerRadius(5)
-            NavigationLink(cellTitle) {
-                destination()
             }
         }
     }
